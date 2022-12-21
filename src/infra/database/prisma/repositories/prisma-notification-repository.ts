591d23d4cdcfdb3f6f9d540de3aad679f3cb9b05
Marcel Barbosa <marcel.sby.br@gmail.com>
2022-12-21 13:06:54 -0300

    feat: lesson 2

---
+++ src/infra/database/prisma/repositories/prisma-notification-repository.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { Notification } from '../../../../application/entities/notification';
+import { NotificationRepository } from '../../../../application/repositories/notification-repository';
+import { PrismaService } from '../prisma.service';
+
+@Injectable()
+export class PrismaNotificationRepository implements NotificationRepository {
+  constructor(private prismaService: PrismaService) {}
+
+  async create({
+    id,
+    content,
+    category,
+    createdAt,
+    readAt,
+    recipientId,
+  }: Notification): Promise<void> {
+    await this.prismaService.notification.create({
+      data: {
+        id,
+        category,
+        content: content.value,
+        recipientId,
+        readAt,
+        createdAt,
+      },
+    });
+  }
+}
